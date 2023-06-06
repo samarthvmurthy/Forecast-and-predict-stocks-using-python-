a94e53c15113ce7d7032d82aa5ce6f86c2265ea2
Samarth <97331528+samarthvmurthy@users.noreply.github.com>
2023-06-06 10:25:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 import streamlit as st
+import pandas as pd
 import yfinance as yf
 import plotly.graph_objs as go
 from datetime import datetime, timedelta
 import requests
-import sqlite3
 
-# Establish a connection to the SQLite database
-conn = sqlite3.connect('user_credentials.db')
-cursor = conn.cursor()
-
-# Create a table to store user credentials if it doesn't exist
-cursor.execute('''CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT)''')
-conn.commit()
+# Define the correct username and password
+correct_username = "admin"
+correct_password = "password"
 
 # Define the layout using Streamlit components
 st.title("Stonks20.com")
@@ -23,49 +19,17 @@ password = st.sidebar.text_input("Password", type="password")
 
 # Check if the user is logged in
 if st.sidebar.button("Login"):
-    cursor.execute("SELECT * FROM users WHERE username=?", (username,))
-    result = cursor.fetchone()
-    if result and result[1] == password:
+    if username == correct_username and password == correct_password:
         st.sidebar.success("Login successful!")
         st.sidebar.info("You can now access other pages.")
         st.session_state["logged_in"] = True
     else:
         st.sidebar.error("Invalid username or password. Please try again.")
 
-# Add a register button
-if st.sidebar.button("Register as a New User"):
-    signup_username = st.sidebar.text_input("New Username")
-    signup_password = st.sidebar.text_input("New Password", type="password")
-    if signup_username and signup_password:
-        register_button = st.sidebar.button("Register")
-        if register_button:
-            cursor.execute("SELECT * FROM users WHERE username=?", (signup_username,))
-            result = cursor.fetchone()
-            if result:
-                st.sidebar.error("Username already exists. Please choose a different username.")
-            else:
-                cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (signup_username, signup_password))
-                conn.commit()
-                st.sidebar.success("Registration successful! Please log in with your new credentials.")
-    else:
-        st.sidebar.warning("Please enter a username and password.")
-
 # Add a logout button to the top right corner
 if st.sidebar.button("Logout"):
     st.session_state.pop("logged_in")
     st.sidebar.success("Logged out successfully!")
-
-def write_user_credentials(username, password):
-    cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
-    conn.commit()
-
-def read_user_credentials():
-    cursor.execute("SELECT * FROM users")
-    rows = cursor.fetchall()
-    user_credentials = {row[0]: row[1] for row in rows}
-    return user_credentials
-
-# Rest of the code remains the same
 
 if st.session_state.get("logged_in"):
     page = st.sidebar.radio("Navigation", ["Stock Analysis", "News"])
@@ -104,7 +68,63 @@ if st.session_state.get("logged_in"):
                 )
                 st.plotly_chart(candlestick_fig)
 
-                # Rest of the code remains the same
+                # Create a Volume chart
+                volume_fig = go.Figure(
+                    data=[go.Bar(x=stock_data.index, y=stock_data["Volume"])]
+                )
+                volume_fig.update_layout(
+                    title="Volume",
+                    xaxis_title="Date",
+                    yaxis_title="Volume",
+                    autosize=True,
+                )
+                st.plotly_chart(volume_fig)
+
+                # Create a Moving Average chart
+                moving_average_fig = go.Figure(
+                    data=[
+                        go.Scatter(
+                            x=stock_data.index, y=stock_data["Close"], name="Price"
+                        ),
+                        go.Scatter(
+                            x=stock_data.index,
+                            y=stock_data["Close"].rolling(window=50).mean(),
+                            name="50-day MA",
+                        ),
+                        go.Scatter(
+                            x=stock_data.index,
+                            y=stock_data["Close"].rolling(window=200).mean(),
+                            name="200-day MA",
+                        ),
+                    ]
+                )
+                moving_average_fig.update_layout(
+                    title="Moving Averages",
+                    xaxis_title="Date",
+                    yaxis_title="Price",
+                    autosize=True,
+                )
+                st.plotly_chart(moving_average_fig)
+
+                # Create an RSI chart
+                delta = stock_data["Close"].diff()
+                gain = delta.mask(delta < 0, 0)
+                loss = -delta.mask(delta > 0, 0)
+                avg_gain = gain.rolling(window=14).mean()
+                avg_loss = loss.rolling(window=14).mean()
+                rs = avg_gain / avg_loss
+                rsi = 100 - (100 / (1 + rs))
+
+                rsi_fig = go.Figure(
+                    data=[go.Scatter(x=stock_data.index, y=rsi, name="RSI", line=dict(color="blue"))]
+                )
+                rsi_fig.update_layout(
+                    title="Relative Strength Index (RSI)",
+                    xaxis_title="Date",
+                    yaxis_title="RSI",
+                    autosize=True,
+                )
+                st.plotly_chart(rsi_fig)
 
             except Exception as e:
                 st.error(f"Error: {str(e)}")
@@ -128,7 +148,3 @@ if st.session_state.get("logged_in"):
 else:
     image_url = "https://i.ytimg.com/vi/if-2M3K1tqk/maxresdefault.jpg"  # Replace with your image URL
     st.image(image_url, use_column_width=True)
-
-# Close the database connection
-cursor.close()
-conn.close()
